14856176cd7cde5f0a1cf2987c8dec9cad885da6
Maxi Hermes <maxi@local>
2026-05-17 05:18:41 -0500

    Install GNOME extensions without D-Bus remote installer

--- install/desktop/set-gnome-extensions.sh
+++ install/desktop/set-gnome-extensions.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -euo pipefail
 
-sudo apt install -y gnome-shell-extension-manager gir1.2-gtop-2.0 gir1.2-clutter-1.0
-pipx install gnome-extensions-cli --system-site-packages
+sudo apt install -y gnome-shell-extension-manager gir1.2-gtop-2.0 gir1.2-clutter-1.0 unzip
+pipx install gnome-extensions-cli --system-site-packages || true
 
 # Turn off Ubuntu/Debian default extensions when present. Missing extensions are OK.
 gnome-extensions disable tiling-assistant@ubuntu.com || true
@@ -12,23 +13,83 @@ gnome-extensions disable ding@rastersoft.com || true
 # Pause to assure user is ready to accept confirmations
 gum confirm "To install Gnome extensions, you need to accept some confirmations. Ready?"
 
-# Install new extensions
-gext install tactile@lundal.io
-gext install just-perfection-desktop@just-perfection
-gext install blur-my-shell@aunetx
-gext install space-bar@luchrioh
-gext install undecorate@sun.wxg@gmail.com
-gext install tophat@fflewddur.github.io
-gext install AlphabeticalAppGrid@stuarthayhurst
+install_extension_zip() {
+  local uuid="$1"
+  local shell_version
+  local download_url
+  local tmpdir
+
+  shell_version="$(gnome-shell --version | grep -oE '[0-9]+' | head -1)"
+  download_url="$(python3 - "$uuid" "$shell_version" <<'PY'
+import json, sys, urllib.parse, urllib.request
+uuid, shell_version = sys.argv[1], sys.argv[2]
+url = "https://extensions.gnome.org/extension-info/?" + urllib.parse.urlencode({"uuid": uuid, "shell_version": shell_version})
+with urllib.request.urlopen(url, timeout=30) as response:
+    data = json.load(response)
+print("https://extensions.gnome.org" + data["download_url"])
+PY
+)"
+
+  tmpdir="$(mktemp -d)"
+  mkdir -p "$HOME/.local/share/gnome-shell/extensions/$uuid"
+  wget -qO "$tmpdir/extension.zip" "$download_url"
+  rm -rf "$HOME/.local/share/gnome-shell/extensions/$uuid"
+  mkdir -p "$HOME/.local/share/gnome-shell/extensions/$uuid"
+  unzip -q "$tmpdir/extension.zip" -d "$HOME/.local/share/gnome-shell/extensions/$uuid"
+  rm -rf "$tmpdir"
+}
+
+# Install new extensions without relying on GNOME Shell's interactive D-Bus remote installer.
+# gext/InstallRemoteExtension can timeout in headless, RDP, or first-login environments even
+# when the extension zip itself is compatible. Direct zip install is deterministic.
+extensions=(
+  'tactile@lundal.io'
+  'just-perfection-desktop@just-perfection'
+  'blur-my-shell@aunetx'
+  'space-bar@luchrioh'
+  'undecorate@sun.wxg@gmail.com'
+  'tophat@fflewddur.github.io'
+  'AlphabeticalAppGrid@stuarthayhurst'
+)
+
+for extension in "${extensions[@]}"; do
+  install_extension_zip "$extension"
+done
 
 # Compile gsettings schemas in order to be able to set them
-sudo cp ~/.local/share/gnome-shell/extensions/tactile@lundal.io/schemas/org.gnome.shell.extensions.tactile.gschema.xml /usr/share/glib-2.0/schemas/
-sudo cp ~/.local/share/gnome-shell/extensions/just-perfection-desktop\@just-perfection/schemas/org.gnome.shell.extensions.just-perfection.gschema.xml /usr/share/glib-2.0/schemas/
-sudo cp ~/.local/share/gnome-shell/extensions/blur-my-shell\@aunetx/schemas/org.gnome.shell.extensions.blur-my-shell.gschema.xml /usr/share/glib-2.0/schemas/
-sudo cp ~/.local/share/gnome-shell/extensions/space-bar\@luchrioh/schemas/org.gnome.shell.extensions.space-bar.gschema.xml /usr/share/glib-2.0/schemas/
-sudo cp ~/.local/share/gnome-shell/extensions/tophat@fflewddur.github.io/schemas/org.gnome.shell.extensions.tophat.gschema.xml /usr/share/glib-2.0/schemas/
-sudo cp ~/.local/share/gnome-shell/extensions/AlphabeticalAppGrid\@stuarthayhurst/schemas/org.gnome.shell.extensions.AlphabeticalAppGrid.gschema.xml /usr/share/glib-2.0/schemas/
+copy_schema() {
+  local schema="$1"
+  local found
+  found="$(find "$HOME/.local/share/gnome-shell/extensions" -path "*/schemas/$schema" -print -quit)"
+  if [[ -n "$found" ]]; then
+    sudo cp "$found" /usr/share/glib-2.0/schemas/
+  fi
+}
+
+copy_schema org.gnome.shell.extensions.tactile.gschema.xml
+copy_schema org.gnome.shell.extensions.just-perfection.gschema.xml
+copy_schema org.gnome.shell.extensions.blur-my-shell.gschema.xml
+copy_schema org.gnome.shell.extensions.space-bar.gschema.xml
+copy_schema org.gnome.shell.extensions.tophat.gschema.xml
+copy_schema org.gnome.shell.extensions.AlphabeticalAppGrid.gschema.xml
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
+
+# Enable extensions by writing GNOME Shell settings directly. This works before the next
+# graphical login and avoids requiring a live org.gnome.Shell.Extensions D-Bus service.
+python3 - <<'PY'
+import subprocess
+extensions = [
+    'undecorate@sun.wxg@gmail.com',
+    'AlphabeticalAppGrid@stuarthayhurst',
+    'space-bar@luchrioh',
+    'just-perfection-desktop@just-perfection',
+    'blur-my-shell@aunetx',
+    'tophat@fflewddur.github.io',
+    'tactile@lundal.io',
+]
+value = '[' + ', '.join(repr(extension) for extension in extensions) + ']'
+subprocess.run(['gsettings', 'set', 'org.gnome.shell', 'enabled-extensions', value], check=True)
+PY
 
 # Configure Tactile
 gsettings set org.gnome.shell.extensions.tactile col-0 1
