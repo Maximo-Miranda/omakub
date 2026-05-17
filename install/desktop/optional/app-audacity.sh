8ac6725b376dc9f9d2ccff681253efe0e80a5502
root <root@vmi3280110.contaboserver.net>
2026-05-17 00:06:50 -0500

    Use sudo for Debian Flatpak installs

--- install/desktop/optional/app-audacity.sh
+++ install/desktop/optional/app-audacity.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-flatpak install -y flathub org.audacityteam.Audacity
+sudo flatpak install -y flathub org.audacityteam.Audacity
--- install/desktop/optional/app-retroarch.sh
+++ install/desktop/optional/app-retroarch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-flatpak install -y flathub org.libretro.RetroArch
+sudo flatpak install -y flathub org.libretro.RetroArch
