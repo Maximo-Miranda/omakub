3d0e3dd2f77989973fc76569a179c01146047a34
root <root@vmi3280110.contaboserver.net>
2026-05-17 00:16:49 -0500

    Guard dash-to-dock settings on Debian GNOME

--- install/desktop/set-gnome-hotkeys.sh
+++ install/desktop/set-gnome-hotkeys.sh
@@ -19,8 +19,11 @@ gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Shift>F11']
 gsettings set org.gnome.mutter dynamic-workspaces false
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 6
 
-# Disable the hotkeys in the Dash to Dock extension (most likely culprit)
-gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
+# Disable the hotkeys in the Dash to Dock extension when the schema exists.
+# Stock Debian GNOME does not ship Ubuntu's dash-to-dock schema.
+if gsettings list-schemas | grep -qx 'org.gnome.shell.extensions.dash-to-dock'; then
+  gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
+fi
 
 # Use alt for pinned apps
 gsettings set org.gnome.shell.keybindings switch-to-application-1 "['<Alt>1']"
